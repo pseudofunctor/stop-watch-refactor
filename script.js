1d99bc06814ac3f04624d46dab291307cdb5ba18
ukarim <13909740+pseudofunctor@users.noreply.github.com>
2019-06-12 06:37:44 -0400

    Modernizing JS file with ES6 syntaxes

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-var seconds = 0
-var timerId = null
-var startButton = document.getElementById("start")
-var pauseButton = document.getElementById("pause")
-var resetButton = document.getElementById("reset")
-var h1 = document.getElementsByTagName("h1")[0]
+let seconds = 0
+let timerId = null
+const startButton = document.getElementById("start")
+const pauseButton = document.getElementById("pause")
+const resetButton = document.getElementById("reset")
+const h1 = document.getElementsByTagName("h1")[0]
 
-startButton.addEventListener("click",function(){
+startButton.addEventListener("click", () => {
   if(!timerId){
     timerId = setInterval(function(){
       h1.innerHTML = "Time Elapsed: " + Math.round(seconds * 100) / 100 
       seconds = seconds += 0.01
-    },10)
+    }, 10)
   }
 })
 
-pauseButton.addEventListener("click",function(){
+pauseButton.addEventListener("click", () => {
   timerId = clearInterval(timerId)
 })
 
-resetButton.addEventListener("click", function(){
+resetButton.addEventListener("click", () => {
   timerId = clearInterval(timerId)
   h1.innerHTML = "Stop Watch"
   seconds = 0
